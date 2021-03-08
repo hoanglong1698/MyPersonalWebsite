9380eaeac12df37c868493c720c371eb4cc8998b
Nguyen Ha Hoang Long <long.nguyen-hoang3@banvien.com.vn>
2021-03-08 14:21:56 +0700

    Change social media

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -8,18 +8,14 @@ const Footer: React.FC = () => (
   <Styled.Footer>
     <Container>
       <Styled.Links>
-        <Styled.Link href="/" rel="noreferrer noopener" target="_blank">
+        <Styled.Link href="https://github.com/hoanglong1698" rel="noreferrer noopener" target="_blank">
           GitHub
         </Styled.Link>
-        <Styled.Link
-          href="https://github.com/SaimirKapaj/gatsby-markdown-typescript-personal-website"
-          rel="noreferrer noopener"
-          target="_blank"
-        >
-          Twitter
+        <Styled.Link href="https://www.linkedin.com/in/long-nguyen-ha-hoang" rel="noreferrer noopener" target="_blank">
+          LinkedIn
         </Styled.Link>
-        <Styled.Link href="/" rel="noreferrer noopener" target="_blank">
-          Behance
+        <Styled.Link href="https://facebook.com/hoanglong1698" rel="noreferrer noopener" target="_blank">
+          Facebook
         </Styled.Link>
       </Styled.Links>
     </Container>
