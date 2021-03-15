e75f3642d9155e6f59e5258cd9605931f4f3bad8
Nguyen Ha Hoang Long <long.nguyen-hoang3@banvien.com.vn>
2021-03-15 14:19:21 +0700

    Change link facebook

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -14,7 +14,7 @@ const Footer: React.FC = () => (
         <Styled.Link href="https://www.linkedin.com/in/long-nguyen-ha-hoang" rel="noreferrer noopener" target="_blank">
           LinkedIn
         </Styled.Link>
-        <Styled.Link href="https://facebook.com/hoanglong1698" rel="noreferrer noopener" target="_blank">
+        <Styled.Link href="https://facebook.com/hoanglong.phmy" rel="noreferrer noopener" target="_blank">
           Facebook
         </Styled.Link>
       </Styled.Links>
